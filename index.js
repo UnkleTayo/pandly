87b0a770b4d2c654bda87f1c64bd5429e392da76
Adetayo <akinsanyaadetayo7@gmail.com>
2021-05-04 17:14:49 +0100

    establish connections

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const { newUser, getIndividualRoomUsers, exitRoom, getActiveUser } = require('./utils/userUtils');
+const formatMessage = require('./utils/parseDate');
+
 
 const app = express()
 
+const server = http.createServer(app)
+const io = socketio(server)
 
-// parse static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000 || process.env.PORT
+// this block will run when the client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = newUser(socket.id, username, room);
 
-app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`))
+    socket.join(user.room);
+
+    // General welcome
+    socket.emit('message', formatMessage("WebCage", 'Messages are limited to this room! '));
+
+    // Broadcast everytime users connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage("WebCage", `${user.username} has joined the room`)
+      );
+
+    // Current active users and room name
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getIndividualRoomUsers(user.room)
+    });
+  });
+
+  // Listen for client message
+  socket.on('chatMessage', msg => {
+    const user = getActiveUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = exitRoom(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage("WebCage", `${user.username} has left the room`)
+      );
+
+      // Current active users and room name
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getIndividualRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
